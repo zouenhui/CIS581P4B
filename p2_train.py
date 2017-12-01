3daff92df9f4140efc277bb4d3b629d5d1a09241
zouenhui <zoue999@gmail.com>
2017-11-30 23:48:00 -0500

    update

---
+++ p2_train.py
@@ -0,0 +1,74 @@
+'''
+  File name: p2_train.py
+  Author:
+  Date:
+'''
+
+import PyNet as net
+from p2_utils import *
+
+
+'''
+  network architecture construction
+  - Stack layers in order based on the architecture of your network
+'''
+
+layer_list = [
+                net.Conv2d(.........),
+                net.BatchNorm2D(.....),
+                net.Relu()
+                ......
+             ]
+
+'''
+  Define loss function
+'''
+loss_layer = net.Loss_Function_name(......)
+
+'''
+  Define optimizer 
+'''
+optimizer = net.optimizer_name(.......)
+
+
+'''
+  Build model
+'''
+my_model = net.Model(layer_list, loss_layer, optimizer)
+
+'''
+  Define the number of input channel and initialize the model
+'''
+my_model.set_input_channel(...)
+
+
+
+'''
+  Main training process
+  - train N epochs, each epoch contains M steps, each step feed a batch-sized data for training,
+    that is, total number of data = M * batch_size, each epoch need to traverse all data.
+'''
+
+# obtain data 
+[data_set, label_set] = loadData(.....)
+
+for i in range (max_epoch_num):
+  '''
+    random shuffle data 
+  '''
+  data_set_cur, label_set_cur = randomShuffle(data_set, label_set) # design function by yourself
+
+
+  step = ...  # step is a int number
+  for j in range (step):
+    # obtain a mini batch for this step training
+    [data_bt, label_bt] = obtainMiniBatch(....)  # design function by yourself
+
+    # feedward data and label to the model
+    loss, pred = my_model(data_bt, label_bt)
+
+    # backward loss
+    my_model.backward(.....)
+
+    # update parameters in model
+    my_model.update_param()
---
+++ p2_utils.py
@@ -0,0 +1,5 @@
+'''
+  File name: p2_utils.py
+  Author:
+  Date:
+'''
